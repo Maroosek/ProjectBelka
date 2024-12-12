41db7fea56ed7b2983f499c4264e8b62a6397554
Maroosek <126722704+Maroosek@users.noreply.github.com>
2024-12-12 17:41:28 +0100

    Added idle mechanic Added idle reward Minor code improvements

--- app/src/main/java/pl/marosek/projectbelka/GameFunction.kt
+++ app/src/main/java/pl/marosek/projectbelka/GameFunction.kt
@@ -1,25 +1,20 @@
 package pl.marosek.projectbelka
 
-import android.widget.TextView
-import android.widget.Toast
-import kotlinx.coroutines.delay
 val gameData = GameScoreData()
 class GameFunction {
     fun addNumber(number: Int, score: Int): Int {
-        var returnScore = score + number
+        val returnScore = score + number
         return returnScore
     }
     fun addNumberPerSecond(number: Int): Int {
         return number / 10
+    }
+    fun addIdleScore(time: Int, scorePerSecond: Int): Int { //function to calculate score when app is closed
+        return time * (scorePerSecond / 10)
     }
 /*    fun buyUpgrade(score: Int, upgradePrice: Int, upgradeValue: Int) {
         score -= upgradePrice
         upgradePrice *= 4
         upgradeValue *= 2
     }*/
-fun startIdle(score : Int, scoreTotalText : TextView) {
-
-    gameData.score += gameData.scorePerSecond
-
-}
 }
--- app/src/main/java/pl/marosek/projectbelka/MainActivity.kt
+++ app/src/main/java/pl/marosek/projectbelka/MainActivity.kt
@@ -2,6 +2,8 @@ package pl.marosek.projectbelka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -23,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     var upgrade5Value = 10000*/
 
     val game = GameFunction()
-    val scoreData = GameScoreData()
+    //val scoreData = GameScoreData()
     val upgradeData = GameUpgradeData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,30 +46,60 @@ class MainActivity : AppCompatActivity() {
         var upgrade5text = findViewById<TextView>(R.id.upgradeText5)
         var upgrade5 = findViewById<Button>(R.id.buttonUpgrade5)
 
+        val idleScoreHandler = Handler(Looper.getMainLooper())
+
+        scorePerSecondText.text = gameData.scorePerSecond.toString()
+
         loadGame()
-        //startIdle(scorePerSecond, scoreTotalText)
+
+        idleScoreHandler.post(object : Runnable {
+            override fun run() {
+                gameData.score += gameData.scorePerSecond
+                refreshText(scoreTotalText, gameData.score)
+                //Toast.makeText(applicationContext, "1 second passed ${gameData.scorePerSecond}", Toast.LENGTH_SHORT).show()
+                idleScoreHandler.postDelayed(this, 1000)
+            }
+        })
+
 
         scoreButton.setOnClickListener {
-            scoreData.score = game.addNumber(upgradeData.upgrade1Value, scoreData.score)
-
-            scoreTotalText.text = scoreData.score.toString()
+            gameData.score = game.addNumber(upgradeData.upgrade1Value, gameData.score)
+            //Only upgrade 1 affects score per click
+            scoreTotalText.text = gameData.score.toString()
         }
 
         upgrade1.setOnClickListener {
-            if (scoreData.score < upgradeData.upgrade1Price) {
+            if (gameData.score < upgradeData.upgrade1Price) {
             Toast.makeText(this, "Not enough cash", Toast.LENGTH_SHORT).show()
 
             return@setOnClickListener
             }
-        scoreData.score -= upgradeData.upgrade1Price
+        gameData.score -= upgradeData.upgrade1Price
         upgradeData.upgrade1Price *= 4
         upgradeData.upgrade1Value *= 2
         upgrade1text.text = upgradeData.upgrade1Price.toString()
-        scoreTotalText.text = scoreData.score.toString()
-        var dupa = game.addNumberPerSecond(upgradeData.upgrade1Value)
-        //addNumberPerSecond(upgradeData.upgrade1Value, scorePerSecondText)
+        scoreTotalText.text = gameData.score.toString()
+        //var dupa = game.addNumberPerSecond(upgradeData.upgrade1Value)
+        gameData.scorePerSecond += game.addNumberPerSecond(upgradeData.upgrade1Value)
+        refreshText(scorePerSecondText, gameData.scorePerSecond)
         //upgrade1text.text = upgrade1Value.toString()
         //upgrade1text.text
+        }
+
+        upgrade2.setOnClickListener {
+            if (gameData.score < upgradeData.upgrade2Price) {
+            Toast.makeText(this, "Not enough cash", Toast.LENGTH_SHORT).show()
+
+            return@setOnClickListener
+            }
+        gameData.score -= upgradeData.upgrade2Price
+        upgradeData.upgrade2Price *= 4
+        upgradeData.upgrade2Value *= 2
+        upgrade2text.text = upgradeData.upgrade2Price.toString()
+        scoreTotalText.text = gameData.score.toString()
+        gameData.scorePerSecond += game.addNumberPerSecond(upgradeData.upgrade2Value)
+        refreshText(scorePerSecondText, gameData.scorePerSecond)
+
         }
     }
 
@@ -86,25 +118,29 @@ class MainActivity : AppCompatActivity() {
         saveGame()
     }
 
-/*    fun addNumberPerSecond(number: Int, text: TextView) {
-        scoreData.scorePerSecond += number / 10
-        text.text = scoreData.scorePerSecond.toString()
-        //refreshText(findViewById(R.id.scorePerSecond), scorePerSecond)
-    }*/
-
     fun refreshText(text: TextView, number: Int) {
         text.text = number.toString()
     }
 
     fun loadGame() {
         val sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE)
-        scoreData.score = sharedPreferences.getInt("score", 0)
-        scoreData.scorePerSecond = sharedPreferences.getInt("scorePerSecond", 0)
+        gameData.score = sharedPreferences.getInt("score", 0)
+        gameData.scorePerSecond = sharedPreferences.getInt("scorePerSecond", 0)
         upgradeData.upgrade1Price = sharedPreferences.getInt("upgrade1Price", 10)
+        upgradeData.upgrade1Value = sharedPreferences.getInt("upgrade1Value", 1)
+        upgradeData.upgrade2Price = sharedPreferences.getInt("upgrade2Price", 100)
+        upgradeData.upgrade2Value = sharedPreferences.getInt("upgrade2Value", 10)
+        //afk time reward
+        var timeAFK = (System.currentTimeMillis() / 1000).toInt() - sharedPreferences.getInt("lastTime", 0)
+        if (timeAFK > 14440) { //4 hours of idle time
+            timeAFK = 14440
+        }
+        Toast.makeText(this, "Time AFK: $timeAFK", Toast.LENGTH_SHORT).show()
+        gameData.score += game.addIdleScore(timeAFK, gameData.scorePerSecond)
         //Add other upgrades
 
-        refreshText(findViewById(R.id.scoreTotal), scoreData.score)
-        refreshText(findViewById(R.id.scorePerSecond), scoreData.scorePerSecond)
+        refreshText(findViewById(R.id.scoreTotal), gameData.score)
+        refreshText(findViewById(R.id.scorePerSecond), gameData.scorePerSecond)
         refreshText(findViewById(R.id.upgradeText1), upgradeData.upgrade1Price)
         refreshText(findViewById(R.id.upgradeText2), upgradeData.upgrade2Price)
         refreshText(findViewById(R.id.upgradeText3), upgradeData.upgrade3Price)
@@ -114,12 +150,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun saveGame() {
+        val currentTime = System.currentTimeMillis() / 1000
         val sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE)
         val editor = sharedPreferences.edit()
-        editor.putInt("score", scoreData.score)
-        editor.putInt("scorePerSecond", scoreData.scorePerSecond)
+        editor.putInt("score", gameData.score)
+        editor.putInt("scorePerSecond", gameData.scorePerSecond)
         editor.putInt("upgrade1Price", upgradeData.upgrade1Price)
         editor.putInt("upgrade1Value", upgradeData.upgrade1Value)
+        editor.putInt("upgrade2Price", upgradeData.upgrade2Price)
+        editor.putInt("upgrade2Value", upgradeData.upgrade2Value)
+        editor.putInt("lastTime", currentTime.toInt())
         //Add other upgrades
         editor.apply()
     }
